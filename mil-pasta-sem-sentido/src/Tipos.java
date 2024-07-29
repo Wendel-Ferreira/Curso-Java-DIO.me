a6000f1c8aafe6278c0a04bb000b02b2c199c9f6
Wendel-Ferreira <wendel124@hotmail.com>
2024-07-29 16:02:18 -0300

    Testando

--- mil-pasta-sem-sentido/src/Tipos.java
+++ mil-pasta-sem-sentido/src/Tipos.java
@@ -1,5 +1,7 @@
 public class Tipos {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       System.out.println("Hello, World!");
+       System.out.println("vamos ver se está sicronizando");
     }
+    
 }
